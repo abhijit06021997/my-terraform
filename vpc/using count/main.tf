d90009119c4087a3805912cc253a74b1933fa755
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2024-12-22 05:10:55 +0530

    Update main.tf

--- vpc/using count/main.tf
+++ vpc/using count/main.tf
@@ -1,1 +1,10 @@
+resource "aws_instance" "web" {
+  ami           = "ami-01816d07b1128cd2d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_type[count.index]
+  }
+  count = 3
+}
 
