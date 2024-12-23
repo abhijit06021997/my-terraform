7849ecc94955cc4c607012d89f1416ae99732462
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2024-12-23 16:51:54 +0530

    Update outputs.tf

--- outputs/outputs.tf
+++ outputs/outputs.tf
@@ -1,4 +1,4 @@
-output "aws_public_ip" {
+output "aws_public_ip" {    # outputs is used to print particular value or resource while creating infrastructure such as public ip,private ip,security name after "terraform apply"
     value = aws_instance.name.public_ip
   
 }
