f715ecb597b008462e8f5b4d72dd863e8a4aa327
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2024-12-23 18:32:20 +0530

    Delete s3/main.tf

--- s3/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "abhijitrateke.site"
-}
-resource "aws_s3_bucket_object" "bucket_data" {
-  bucket = aws_s3_bucket.my_bucket.bucket #it denoted in which bucket you are uploading  as well as "bucket=abhijitramteke.site thats specified  [hole bucketb name in upper content]
-  source = "./object.txt"
-  key    = "object.txt" #key represented where we want to upload [it is compulsory]
-}
