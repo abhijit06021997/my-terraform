80ac1928cd4602b91cc376b697580811e90ce860
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2024-12-23 10:26:18 +0530

    Delete instance/variable.tf

--- instance/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_type" {
-  type = list(string)
-  default = [ "t2.nano", "t2.micro" ]
-  
-}
-variable "instance_name" {
-  type = list(string)
-default = [ "webser-1", "webserver-2" ]
-}
