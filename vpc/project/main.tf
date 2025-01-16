21f513d83f8e71e83f437d639a1018d9e12ed4da
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-16 20:29:08 +0530

    Update main.tf

--- vpc/project/main.tf
+++ vpc/project/main.tf
@@ -1,52 +1,40 @@
-
-resource "aws_instance" "my_instance"{
-  instance_type               = "t2.micro"
-  ami                         = "ami-0cde6390e61a03eee"
-  subnet_id                   = aws_subnet.my-subne-pub.id
-  vpc_security_group_ids      = ["aws_security_group.example.id"]
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-#!/bin/bash
-sudo apt-get update -y
-sudo apt install nginx
-sudo systemctl start nginx    
-EOF
-
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    name = "sample"
+    name = "myvpc-1"
   }
 }
 
-resource "aws_security_group" "example" {
-  name        = "example"
-  description = "example"
-  vpc_id      = aws_vpc.my-vpc.id
+resource "aws_subnet" "my-subne-pub" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
   tags = {
-    Name = "example"
-  }
-
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    protocol    = "tcp"
-    to_port     = 80
-  }
-
-
-
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
+    name = "mysubnet"
   }
 
 }
 
+resource "aws_internet_gateway" "my-intenetgateway" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    name = "my-inrtergegateway"
+  }
+}
 
+resource "aws_route_table" "myroute-table" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-intenetgateway.id
+  }
+}
+
+resource "aws_route_table_association" "route-table-association" {
+  route_table_id = aws_route_table.myroute-table.id
+  subnet_id      = aws_subnet.my-subne-pub.id
+
+}
 
 
 
