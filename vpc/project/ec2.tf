e9f8fc8537376ea71197a3ed3f1a11f1a6af9857
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-17 21:21:55 +0530

    Update ec2.tf

--- vpc/project/ec2.tf
+++ vpc/project/ec2.tf
@@ -1,52 +1,41 @@
-
-resource "aws_instance" "my_instance"{
+resource "aws_instance" "ec2" {
+  ami                         = "ami-01816d07b1128cd2d"
   instance_type               = "t2.micro"
-  ami                         = "ami-0cde6390e61a03eee"
-  subnet_id                   = aws_subnet.my-subne-pub.id
-  vpc_security_group_ids      = ["aws_security_group.example.id"]
+  subnet_id                   = aws_subnet.my-subnet-pub.id
   associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.my-sec.id]
+
+
 
   user_data = <<-EOF
-#!/bin/bash
-sudo apt-get update -y
-sudo apt install nginx
-sudo systemctl start nginx    
-EOF
-
-  tags = {
-    name = "sample"
-  }
+  #!/bin/bash
+  sudo yum update -y 
+  sudo yum install nginx -y
+  sudo systemctl start nginx 
+  EOF
 }
 
-resource "aws_security_group" "example" {
-  name        = "example"
-  description = "example"
-  vpc_id      = aws_vpc.my-vpc.id
-  tags = {
-    Name = "example"
-  }
-
+resource "aws_security_group" "my-sec" {
+  vpc_id = aws_vpc.my-vpc.id
 
   ingress {
+    to_port     = 22
+    from_port   = 22
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    protocol    = "tcp"
-    to_port     = 80
   }
 
-
-
+  ingress {
+    to_port     = 80
+    from_port   = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-    cidr_blocks = ["0.0.0.0/0"]
+    to_port     = 0
     from_port   = 0
     protocol    = "-1"
-    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
-
-
-
-
-
